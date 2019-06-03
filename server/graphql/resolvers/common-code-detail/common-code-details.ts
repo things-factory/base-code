9c86d40a76af731fd886073e8470a5b90cfe5f63
leemyunggyu <jaylee@hatiolab.com>
2019-06-03 15:07:47 +0900

    v0.1.15

--- server/graphql/resolvers/common-code-detail/common-code-details.ts
+++ server/graphql/resolvers/common-code-detail/common-code-details.ts
@@ -1,9 +1,9 @@
-import { buildQuery, ListParams } from '@things-factory/shell'
+import { buildQuery, ListParam } from '@things-factory/shell'
 import { getRepository } from 'typeorm'
 import { CommonCodeDetail } from '../../../entities'
 
 export const commonCodeDetailsResolver = {
-  async commonCodeDetails(_: any, params: ListParams, context: any) {
+  async commonCodeDetails(_: any, params: ListParam, context: any) {
     const queryBuilder = getRepository(CommonCodeDetail).createQueryBuilder()
     buildQuery(queryBuilder, params)
     const [items, total] = await queryBuilder.getManyAndCount()
--- server/graphql/resolvers/common-code/common-codes.ts
+++ server/graphql/resolvers/common-code/common-codes.ts
@@ -1,9 +1,9 @@
-import { buildQuery, ListParams } from '@things-factory/shell'
+import { buildQuery, ListParam } from '@things-factory/shell'
 import { getRepository } from 'typeorm'
 import { CommonCode } from '../../../entities'
 
 export const commonCodesResolver = {
-  async commonCodes(_: any, params: ListParams, context: any) {
+  async commonCodes(_: any, params: ListParam, context: any) {
     const queryBuilder = getRepository(CommonCode).createQueryBuilder()
     buildQuery(queryBuilder, params)
     const [items, total] = await queryBuilder.getManyAndCount()
