a2ecb42a09ab9e4243647ab2777121320f25e5c5
leemyunggyu <jaylee@hatiolab.com>
2019-09-30 10:59:16 +0800

    v0.1.43

--- server/graphql/resolvers/common-code-detail/delete-common-code-details.ts
+++ server/graphql/resolvers/common-code-detail/delete-common-code-details.ts
@@ -2,7 +2,7 @@ import { getRepository, In } from 'typeorm'
 import { CommonCodeDetail } from '../../../entities'
 
 export const deleteCommonCodeDetails = {
-  async deleteCommonCodesDetails(_: any, { names }, context: any) {
+  async deleteCommonCodeDetails(_: any, { names }, context: any) {
     await getRepository(CommonCodeDetail).delete({
       domain: context.state.domain,
       name: In(names)
