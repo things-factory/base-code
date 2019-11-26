c3f138780b5826679f632f1554e8530af612cb4a
leemyunggyu <jaylee@hatiolab.com>
2019-11-26 16:35:28 +0900

    v1.0.0-alpha.5

--- server/graphql/types/common-code-detail/common-code-detail-patch.ts
+++ server/graphql/types/common-code-detail/common-code-detail-patch.ts
@@ -2,6 +2,7 @@ import gql from 'graphql-tag'
 
 export const CommonCodeDetailPatch = gql`
   input CommonCodeDetailPatch {
+    id: String
     name: String
     commonCode: ObjectRef
     description: String
--- server/graphql/types/common-code/common-code-patch.ts
+++ server/graphql/types/common-code/common-code-patch.ts
@@ -2,6 +2,7 @@ import gql from 'graphql-tag'
 
 export const CommonCodePatch = gql`
   input CommonCodePatch {
+    id: String
     name: String
     description: String
     details: [String]
